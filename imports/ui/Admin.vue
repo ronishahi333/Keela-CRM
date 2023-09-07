0ca07c0c2fab214e1426c0095a0173237d8ee2b7
ronnie <ronish.shahi@keela.com>
2023-09-07 22:15:20 +0545

    Added Modal for 'Add Organization'

--- imports/ui/Admin.vue
+++ imports/ui/Admin.vue
@@ -118,15 +118,140 @@
     </div>
   </div>
 
-  <!-- Add Organizations Button -->
+  <!-- Add Organizations Button with Modal-->
+  
   <div class="grid grid-cols-6">
-    <div class="col-start-6 text-right">
-      <button
-        type="button"
-        class="px-5 py-2 focus:outline-none text-white bg-purple-700 hover:bg-purple-800 focus:ring-4 focus:ring-purple-300 font-medium rounded-lg text-sm mb-2 dark:bg-purple-600 dark:hover:bg-purple-700 dark:focus:ring-purple-900"
-      >
-        Add Organizations
-      </button>
+    <div class="col-start-6 justify-self-end">
+      <div>
+        <!-- Modal toggle button -->
+        <button
+          data-modal-toggle="authentication-modal"
+          class="px-5 py-2 focus:outline-none text-white bg-purple-700 hover:bg-purple-800 focus:ring-4 focus:ring-purple-300 font-medium rounded-lg text-sm mb-2 dark:bg-purple-600 dark:hover:bg-purple-700 dark:focus:ring-purple-900"
+          type="button"
+          @click="toggleModal"
+        >
+          Add Organization
+        </button>
+    
+        <!-- Overlay -->
+        <div
+          v-if="isModalOpen"
+          class="fixed inset-0 z-40 bg-gray-900 opacity-50 flex items-center justify-center"
+        ></div>
+
+        <!-- Main modal -->
+        <div
+          id="authentication-modal"
+          :class="{ hidden: !isModalOpen }"
+          tabindex="-1"
+          aria-hidden="true"
+          class="fixed top-0 left-0 right-0 z-50 w-full p-4 overflow-x-hidden overflow-y-auto md:inset-0 h-[calc(100%-1rem)] max-h-full flex items-center justify-center"
+        >
+          <!-- Modal content -->
+          <div class="relative w-full max-w-md max-h-full">
+            <!-- Modal content -->
+            <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
+              <button
+                type="button"
+                class="absolute top-3 right-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ml-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white"
+                @click="toggleModal"
+              >
+                <svg
+                  class="w-3 h-3"
+                  aria-hidden="true"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 14 14"
+                >
+                  <path
+                    stroke="currentColor"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="2"
+                    d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"
+                  />
+                </svg>
+                <span class="sr-only">Close modal</span>
+              </button>
+              <div class="px-6 py-6 lg:px-8">
+                <h3
+                  class="mb-8 text-xl font-medium text-gray-900 dark:text-white"
+                >
+                  Register your Organization
+                </h3>
+                <form class="space-y-6" action="#">
+                  <div>
+                    <label
+                      for="organization"
+                      class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                      >Organization Name</label
+                    >
+                    <input
+                      type="text"
+                      name="organization"
+                      id="organization"
+                      class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
+                      required
+                      placeholder="Social Donation"
+                    />
+                  </div>
+                  <div>
+                    <label
+                      for="fullname"
+                      class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                      >Full Name</label
+                    >
+                    <input
+                      type="text"
+                      name="fullname"
+                      id="fullname"
+                      class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
+                      required
+                      placeholder="Marcus Stoinis"
+                    />
+                  </div>
+                  <div>
+                    <label
+                      for="email"
+                      class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                      >Email</label
+                    >
+                    <input
+                      type="email"
+                      name="email"
+                      id="email"
+                      class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
+                      placeholder="name@company.com"
+                      required
+                    />
+                  </div>
+                  <div>
+                    <label
+                      for="password"
+                      class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                      >Password</label
+                    >
+                    <input
+                      type="password"
+                      name="password"
+                      id="password"
+                      placeholder="••••••••"
+                      class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
+                      required
+                    />
+                  </div>
+                  <button
+                    type="submit"
+                    class="w-full text-white bg-purple-700 hover:bg-purple-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                  >
+                    Register
+                  </button>
+                </form>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 
@@ -179,6 +304,22 @@
     </div>
   </div>
 </template>
+
 <script>
-export default {};
+import { ref } from "vue";
+
+export default {
+  setup() {
+    const isModalOpen = ref(false);
+
+    function toggleModal() {
+      isModalOpen.value = !isModalOpen.value;
+    }
+
+    return {
+      isModalOpen,
+      toggleModal,
+    };
+  },
+};
 </script>
--- server/main.js
+++ server/main.js
@@ -1,31 +1,44 @@
-import { Meteor } from 'meteor/meteor'
-import { LinksCollection } from '/imports/api/links'
+import { Meteor } from "meteor/meteor";
+import { Accounts } from "meteor/accounts-base";
+import { LinksCollection } from "/imports/api/links";
+
+const SEED_USERNAME = "Ronnie";
+const SEED_PASSWORD = "Ronnie";
+
+Meteor.startup(() => {
+  if (!Accounts.findUserByUsername(SEED_USERNAME)) {
+    Accounts.createUser({
+      username: SEED_USERNAME,
+      password: SEED_PASSWORD,
+    });
+  }
+});
 
 async function insertLink({ title, url }) {
-  await LinksCollection.insertAsync({ title, url, createdAt: new Date() })
+  await LinksCollection.insertAsync({ title, url, createdAt: new Date() });
 }
 
 Meteor.startup(async () => {
   // If the Links collection is empty, add some data.
   if ((await LinksCollection.find().countAsync()) === 0) {
     await insertLink({
-      title: 'Do the Tutorial',
-      url: 'https://vuejs.org/guide/quick-start.html',
-    })
+      title: "Do the Tutorial",
+      url: "https://vuejs.org/guide/quick-start.html",
+    });
 
     await insertLink({
-      title: 'Follow the Guide',
-      url: 'https://guide.meteor.com',
-    })
+      title: "Follow the Guide",
+      url: "https://guide.meteor.com",
+    });
 
     await insertLink({
-      title: 'Read the Docs',
-      url: 'https://docs.meteor.com',
-    })
+      title: "Read the Docs",
+      url: "https://docs.meteor.com",
+    });
 
     await insertLink({
-      title: 'Discussions',
-      url: 'https://forums.meteor.com',
-    })
+      title: "Discussions",
+      url: "https://forums.meteor.com",
+    });
   }
-})
+});
